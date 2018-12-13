c23e9d2a4855f36781d4a8bdb8cdb15a43a5dcc5
phichayapa <59070133@it.kmitl.ac.th>
2018-12-13 22:26:24 +0700

    Add files via upload

---
+++ Oil price information/py_project.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+df_collect = pd.read_excel('2554.xlsx', sheet_name = 'ค่าเฉลี่ยราคาน้ำมันแต่ละเดือน')
+List_x = df_collect['month'].tolist()
+List_y1 = df_collect['Gasohol 91'].tolist()
+List_y2 = df_collect['Gasohol 95'].tolist()
+List_y3 = df_collect['Gasohol E20'].tolist()
+List_y4 = df_collect['Gasohol E85'].tolist()
+List_y5 = df_collect['Ultra Force Diesel'].tolist()
+plt.plot(List_x, List_y1, label='Gasohol 91')
+plt.plot(List_x, List_y2, label='Gasohol 95')
+plt.plot(List_x, List_y3, label='Gasohol E20')
+plt.plot(List_x, List_y4, label='Gasohol E85')
+plt.plot(List_x, List_y5, label='Ultra Force Diesel')
+plt.xlabel('month')
+plt.ylabel('price')
+plt.legend()
+plt.show()
