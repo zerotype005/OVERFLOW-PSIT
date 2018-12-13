b698b80c5cb33d48d43bd8a7c32c278e81aaa65f
phichayapa <59070133@it.kmitl.ac.th>
2018-12-13 22:25:53 +0700

    Delete py_project.py

--- Oil price information/py_project.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-df_collect = pd.read_excel('2554.xlsx', sheet_name = 'ค่าเฉลี่ยราคาน้ำมันแต่ละเดือน')
-List_x = df_collect['month'].tolist()
-List_y = df_collect['Gasohol 91'].tolist()
-plt.plot(List_x, List_y)
-plt.xlabel('month')
-plt.ylabel('price')
