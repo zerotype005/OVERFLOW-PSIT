d07bc8c8da910c014b3d851536893f221f0ef012
ptydvp <43006822+ptydvp@users.noreply.github.com>
2018-12-17 13:46:22 +0700

    Add files via upload

---
+++ Python/2555.py
@@ -0,0 +1,23 @@
+import csv
+import pygal
+data55 = csv.reader(open('2555.txt'))
+oil1 = []
+price = []
+oil2 = []
+for i in data55:
+    oil1.append(i)
+for i in oil1:
+    oil2.append(i[0])
+for i in oil1:
+    price.append(i[1])
+print(oil2)
+print(price)
+line = pygal.Bar()
+line.title = "ราคาน้ำมันเฉลี่ยของปี 2555"
+line.x_labels = ("")
+line.add(oil2[0], float(price[0]))
+line.add(oil2[1], float(price[1]))
+line.add(oil2[2], float(price[2]))
+line.add(oil2[3], float(price[3]))
+line.add(oil2[4], float(price[4]))
+line.render_to_file('2555.svg')\
---
+++ Python/2559.py
@@ -0,0 +1,24 @@
+import csv
+import pygal
+data55 = csv.reader(open('2559.txt'))
+oil1 = []
+price = []
+oil2 = []
+for i in data55:
+    oil1.append(i)
+for i in oil1:
+    oil2.append(i[0])
+for i in oil1:
+    price.append(i[1])
+print(oil2)
+print(price)
+line = pygal.Bar()
+line.title = "ราคาน้ำมันเฉลี่ยของปี 2559"
+line.x_labels = ("")
+line.y_title = ('Price')
+line.add(oil2[0], float(price[0]))
+line.add(oil2[1], float(price[1]))
+line.add(oil2[2], float(price[2]))
+line.add(oil2[3], float(price[3]))
+line.add(oil2[4], float(price[4]))
+line.render_to_file('2559.svg')
